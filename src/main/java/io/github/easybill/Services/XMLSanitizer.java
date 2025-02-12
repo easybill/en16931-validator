51dc735c940e3cc5d4a78dd0ed53f206dbdb2f11
Jan Noehles <jan.noehles@easybill.de>
2025-02-12 08:20:43 +0100

    0.4.1 - Improve error handling. Return 422 if parse exception occurs (#9)
    * Throw XmlInvalidException if a SAXParse error occurs and return 422

--- src/main/java/io/github/easybill/Services/XMLSanitizer.java
+++ src/main/java/io/github/easybill/Services/XMLSanitizer.java
@@ -1,5 +1,6 @@
 package io.github.easybill.Services;
 
+import io.github.easybill.Exceptions.InvalidXmlException;
 import io.github.easybill.Exceptions.XmlSanitizationException;
 import java.io.*;
 import java.nio.charset.Charset;
@@ -18,6 +19,7 @@ import org.w3c.dom.*;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 public final class XMLSanitizer {
 
@@ -30,6 +32,8 @@ public final class XMLSanitizer {
                 removeInvalidCharsFromProlog(removeBOM(xml)),
                 charset
             );
+        } catch (SAXParseException parseException) {
+            throw new InvalidXmlException(parseException);
         } catch (Exception exception) {
             throw new XmlSanitizationException(exception);
         }
